f11a469dddf3ff1a9b757249651c0c8f7ee61d99
Peter Hrincar <peter.hrincar@gmail.com>
2017-01-21 14:33:19 +0100

    Add test scene

---
+++ Assets/Scripts/Breaks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breaks : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log ("space");
+            boxCollider2D.enabled = true;
+        } 
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            boxCollider2D.enabled = false;
+        }
+	}
+}
