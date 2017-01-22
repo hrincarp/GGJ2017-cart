ca7f6ec4b3755e1b050089efc2d3454eea1bf3f3
Peter Hrincar <peter.hrincar@gmail.com>
2017-01-22 06:36:10 +0100

    Breaks

---
+++ Assets/RotationMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            Rigidbody rigidbody = GetComponent<Rigidbody> ();
+            rigidbody.AddTorque (new Vector3 (0f, 0f, -100f));
+            Debug.Log ("test");
+        }
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            Rigidbody rigidbody = GetComponent<Rigidbody> ();
+            rigidbody.AddTorque (new Vector3 (0f, 0f, 100f));
+            Debug.Log ("test");
+        }
+	}
+    void FixedUpdate() { 
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+
+        //Set non essential angles to 0.
+        eulerRotation.x = 0;
+        eulerRotation.y = 0;
+
+        //Reset the transform rotation.
+        transform.rotation = Quaternion.Euler(eulerRotation);
+
+        Vector3 pos = transform.position;
+        pos.z = 0;
+        transform.position = pos;
+    }
+}
--- Assets/Scripts/Breaks.cs
+++ Assets/Scripts/Breaks.cs
@@ -2,27 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breaks : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
+public class Breaks : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
 		
-	}
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update ()
+    {
 
-        Collider collider = GetComponent<Collider>();
-        if (Input.GetKey(KeyCode.Space)) {
+        Collider collider = GetComponent<Collider> ();
+        if (Input.GetKey (KeyCode.Space) && canBreak) {
             Debug.Log (collider.material.staticFriction);
-            collider.material.staticFriction = Mathf.Lerp(collider.material.staticFriction, 100, 0.01f * Time.deltaTime);
+            //collider.material.staticFriction = Mathf.Lerp(collider.material.staticFriction, 10, 0.1f * Time.deltaTime);
+            collider.material.dynamicFriction = Mathf.Lerp (collider.material.dynamicFriction, 10, 0.1f * Time.deltaTime);
 
             collider.material = collider.material;
+
+            var emission = GetComponent<ParticleSystem> ().emission;
+    
+            var rate = emission.rate;
+            rate.constantMax = 10 * GetComponentInParent<Rigidbody> ().velocity.magnitude;
+            emission.rate = rate;
+           
             //collider.enabled = true;
         } else {
-        //if (Input.GetKeyUp(KeyCode.Space)) {
-            collider.material.staticFriction = Mathf.Lerp(collider.material.staticFriction, 0, 0.1f * Time.deltaTime);
+            //if (Input.GetKeyUp(KeyCode.Space)) {
+            //collider.material.staticFriction = Mathf.Lerp(collider.material.staticFriction, 0, 0.1f * Time.deltaTime);
+            collider.material.dynamicFriction = Mathf.Lerp (collider.material.dynamicFriction, 0, 0.5f * Time.deltaTime);
             //collider.enabled = false;
             collider.material = collider.material;
+
+            var emission = GetComponent<ParticleSystem> ().emission;
+
+            var rate = emission.rate;
+            rate.constantMax = 0;
+            emission.rate = rate;
+
+
+            //GetComponent<ParticleSystem> ().Stop ();
         }
-	}
+        //canBreak = false;
+           
+    }
+    private bool canBreak = false;
+
+        void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.tag == "level") {
+            canBreak = true;
+        }
+      
+        }
+    void OnTriggerExit(Collider other) {
+
+        if (other.gameObject.tag == "level") {
+            canBreak = false;
+        }
+
+    }
 }
