3887eb0dfe78f6e1d09ac0542eca073698841e76
Peter Hrincar <peter.hrincar@gmail.com>
2017-01-22 09:53:23 +0100

    You win

---
+++ Assets/Finnish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finnish : MonoBehaviour {
+    public Text youWinText;
+
+	// Use this for initialization
+	void Start () {
+        youWinText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log (collider.gameObject.tag);
+        if (collider.gameObject.tag == "Player") {
+            Debug.Log ("win");
+            youWinText.enabled = true;
+        }
+    }
+}
--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -17,7 +17,7 @@ public class SmoothCamera2D : MonoBehaviour {
             
             Camera camera = GetComponent<Camera>();
             Vector3 point = camera.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 15.5f));// point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 25.5f));// point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
