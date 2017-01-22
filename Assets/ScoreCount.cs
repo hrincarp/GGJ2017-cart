30e919bf71ec707d8d7a849ca8cb5783ff15788c
Peter Hrincar <peter.hrincar@gmail.com>
2017-01-22 08:30:05 +0100

    Score

---
+++ Assets/ScoreCount.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour {
+    public Text text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    int score = 0;
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "gem") {
+            score++;
+            text.text = score.ToString();
+        }
+    }
+
+    void OnTriggerExit(Collider collider) {
+        if (collider.gameObject.tag == "gem") {
+            score--;
+            text.text = score.ToString();
+        }
+    }
+}
